940ef68c1e8b621906ce1162f80baecb458adf31
terejoe <nzeamalunkechinyere@gmail.com>
2022-08-05 18:33:43 +0100

    responsive build

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let sidebar = document.getElementById("min-navlist");
+ 
+const openFunction = () =>{
+  sidebar.style.display = "block";
+}
+
+const closeFunction = () =>{
+  sidebar.style.display = "none";
+}
+
